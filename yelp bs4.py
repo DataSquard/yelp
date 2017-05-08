9ac93c17edc31d6a6880242996fbbc51fafff8b3
libgy24 <lizhengyizak@gmail.com>
2017-05-07 22:56:01 -0500

    add a basic version to get basic restaurants infor

--- yelp bs4.py
+++ yelp bs4.py
@@ -5,6 +5,79 @@ import pandas as pd
 
 baseUrl = 'https://www.yelp.com/search?find_loc=Eden+Prairie,+MN&start=0&cflt=restaurants'
 
+def YelpSpiderBasic(baseUrl):
+    baseUrl_html = urlopen(baseUrl)
+    baseUrl_Obj = BeautifulSoup(baseUrl_html)
+
+    num_of_pages = int(re.findall('^.*\s([0-9]+)$',
+                                  baseUrl_Obj.find('div', {
+                                      'class': 'page-of-pages arrange_unit arrange_unit--fill'}).text.strip())[0])
+    YelpRestaurant = {}
+
+    for page_num in range(0, num_of_pages):
+        SearchUrl = 'https://www.yelp.com/search?find_loc=Eden+Prairie,+MN&start={}&cflt=restaurants'.format(
+            page_num * 10)
+        SearchUrl_html = urlopen(SearchUrl)
+        SearchUrl_Obj = BeautifulSoup(SearchUrl_html)
+
+        for RestInfo in SearchUrl_Obj.find_all('li', {'class': 'regular-search-result'}):
+            if RestInfo.find('a', {'class': 'biz-name js-analytics-click'}).span is not None:
+                RestName = RestInfo.find('a', {'class': 'biz-name js-analytics-click'}).span.text
+            else:
+                RestName = 'Unknown'
+
+            if RestInfo.find('div', {'class': 'biz-rating biz-rating-large clearfix'}) is None:
+                rating = -1.0
+                num_of_reviews = 0
+            else:
+                rating = float(re.findall('\d+\.\d+',
+                                          RestInfo.find('div',
+                                                        {'class': 'biz-rating biz-rating-large clearfix'}).div.attrs[
+                                              'title'])[0])
+                num_of_reviews = int(re.findall('^\s+([0-9]+)\s.*',
+                                                RestInfo.find('div', {
+                                                    'class': 'biz-rating biz-rating-large clearfix'}).span.text)[0])
+
+            if RestInfo.find('div', {'class': 'secondary-attributes'}).address is None:
+                address_1 = 'Unknown'
+                address_2 = 'Unknown'
+            elif len(RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents) == 3:
+                address_1 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[0].strip()
+                address_2 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[2].strip()
+            else:
+                address_1 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[0].strip()
+                address_2 = 'Unknown'
+
+            if RestInfo.find('span', {'class': 'biz-phone'}).text is None or len(
+                    RestInfo.find('span', {'class': 'biz-phone'}).text.strip()) == 0:
+                phone = 9999999999
+            else:
+                phone = int(''.join(re.findall('\d+', RestInfo.find('span', {'class': 'biz-phone'}).text)))
+
+            if RestInfo.find('span', {'class': 'category-str-list'}).a is None:
+                category = 'Unknown'
+            else:
+                category = RestInfo.find('span', {'class': 'category-str-list'}).a.text
+
+            if RestInfo.find('span', {'class': 'business-attribute price-range'}) is None:
+                price_range = -1
+            else:
+                price_range = len(RestInfo.find('span', {'class': 'business-attribute price-range'}).text)
+
+            YelpRestaurant[RestName] = [rating, num_of_reviews, address_1, address_2,
+                                        phone, category, price_range]
+
+            Yelp_df = pd.DataFrame.from_dict(YelpRestaurant, orient='index').rename(columns={0: 'rating',
+                                                                                             1: 'num_of_reviews',
+                                                                                             2: 'address_1',
+                                                                                             3: 'address_2',
+                                                                                             4: 'phone',
+                                                                                             5: 'category',
+                                                                                             6: 'price_range'})
+    return Yelp_df
+
+
+
 
 def YelpSpider(baseUrl):
     baseUrl_html = urlopen(baseUrl)
@@ -80,73 +153,5 @@ def YelpSpider(baseUrl):
     return YelpRestaurant
 
 
-def YelpSpiderBasic(baseUrl):
-    baseUrl_html = urlopen(baseUrl)
-    baseUrl_Obj = BeautifulSoup(baseUrl_html)
 
-    num_of_pages = int(re.findall('^.*\s([0-9]+)$',
-                                  baseUrl_Obj.find('div', {
-                                      'class': 'page-of-pages arrange_unit arrange_unit--fill'}).text.strip())[0])
-    YelpRestaurant = {}
-
-    for page_num in range(0, num_of_pages):
-        SearchUrl = 'https://www.yelp.com/search?find_loc=Eden+Prairie,+MN&start={}&cflt=restaurants'.format(
-            page_num * 10)
-        SearchUrl_html = urlopen(SearchUrl)
-        SearchUrl_Obj = BeautifulSoup(SearchUrl_html)
-
-        for RestInfo in SearchUrl_Obj.find_all('li', {'class': 'regular-search-result'}):
-            if RestInfo.find('a', {'class': 'biz-name js-analytics-click'}).span is not None:
-                RestName = RestInfo.find('a', {'class': 'biz-name js-analytics-click'}).span.text
-            else:
-                RestName = 'Unknown'
-
-            if RestInfo.find('div', {'class': 'biz-rating biz-rating-large clearfix'}) is None:
-                rating = -1.0
-                num_of_reviews = 0
-            else:
-                rating = float(re.findall('\d+\.\d+',
-                                          RestInfo.find('div',
-                                                        {'class': 'biz-rating biz-rating-large clearfix'}).div.attrs[
-                                              'title'])[0])
-                num_of_reviews = int(re.findall('^\s+([0-9]+)\s.*',
-                                                RestInfo.find('div', {
-                                                    'class': 'biz-rating biz-rating-large clearfix'}).span.text)[0])
-
-            if RestInfo.find('div', {'class': 'secondary-attributes'}).address is None:
-                address_1 = 'Unknown'
-                address_2 = 'Unknown'
-            elif len(RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents) == 3:
-                address_1 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[0].strip()
-                address_2 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[2].strip()
-            else:
-                address_1 = RestInfo.find('div', {'class': 'secondary-attributes'}).address.contents[0].strip()
-                address_2 = 'Unknown'
-
-            if RestInfo.find('span', {'class': 'biz-phone'}).text is None or len(
-                    RestInfo.find('span', {'class': 'biz-phone'}).text.strip()) == 0:
-                phone = 9999999999
-            else:
-                phone = int(''.join(re.findall('\d+', RestInfo.find('span', {'class': 'biz-phone'}).text)))
-
-            if RestInfo.find('span', {'class': 'category-str-list'}).a is None:
-                category = 'Unknown'
-            else:
-                category = RestInfo.find('span', {'class': 'category-str-list'}).a.text
-
-            if RestInfo.find('span', {'class': 'business-attribute price-range'}) is None:
-                price_range = -1
-            else:
-                price_range = len(RestInfo.find('span', {'class': 'business-attribute price-range'}).text)
-
-            YelpRestaurant[RestName] = [rating, num_of_reviews, address_1, address_2,
-                                        phone, category, price_range]
-
-            Yelp_df = pd.DataFrame.from_dict(YelpRestaurant, orient='index').rename(columns={0: 'rating',
-                                                                                             1: 'num_of_reviews',
-                                                                                             2: 'address_1',
-                                                                                             3: 'address_2',
-                                                                                             4: 'phone',
-                                                                                             5: 'category',
-                                                                                             6: 'price_range'})
-    return Yelp_df
+eden_df = YelpSpiderBasic(baseUrl)
